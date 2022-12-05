effd28530a439d042995f38fd5ec0a7053acba7b
MattForgione <mattforgionedev@gmail.com>
2022-12-05 14:51:51 -0400

    minor tweaks
    - added status codes to responses
    - changed username to email in jwt strategy

--- todo-api/src/auth/auth.controller.ts
+++ todo-api/src/auth/auth.controller.ts
@@ -7,6 +7,8 @@ import {
   Get,
   Patch,
   Param,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -31,32 +33,38 @@ export class AuthController {
 
   @UseGuards(LoginAuthGuard)
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   async login(@Request() req) {
     return await this.authService.login(req.user);
   }
 
   @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
   async signup(@Body() createUser: CreateUserDto) {
     return this.authService.signup(createUser.email, createUser.password);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('user')
+  @HttpCode(HttpStatus.OK)
   async getProfile(@Request() req) {
     return req.user;
   }
 
   @Post('verify-email')
+  @HttpCode(HttpStatus.OK)
   async verifyEmail(@Body() body: VerifyEmailToken) {
     return this.authService.verifyEmail(body.token);
   }
 
   @Post('password-reset')
+  @HttpCode(HttpStatus.OK)
   async resetPasswordEmail(@Body() body: ResetPasswordEmail) {
     return await this.authService.sendResetPasswordEmail(body.email);
   }
 
   @Patch('password-reset-form/:token')
+  @HttpCode(HttpStatus.OK)
   async passwordResetFormSubmission(
     @Param('token') token: string,
     @Body() body: ResetPasswordFormSubmission
