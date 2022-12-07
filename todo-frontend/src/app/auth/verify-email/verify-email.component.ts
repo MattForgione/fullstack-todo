f05070c7791b058936ced2310440255c87249a9c
MattForgione <mattforgionedev@gmail.com>
2022-12-07 11:22:52 -0400

    refactored the verify-email.component.ts to be more a more idiomatic rxjs stream without the use of nested subscribes

--- todo-frontend/src/app/auth/verify-email/verify-email.component.ts
+++ todo-frontend/src/app/auth/verify-email/verify-email.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { CookieService } from 'ngx-cookie-service';
+import { mergeMap } from 'rxjs';
 
 @Component({
   selector: 'app-verify-email',
@@ -10,6 +11,7 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class VerifyEmailComponent implements OnInit {
   status!: string;
+  token!: string;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -19,28 +21,24 @@ export class VerifyEmailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.verify();
-  }
+    this.activatedRoute.paramMap
+      .pipe(
+        mergeMap(params => {
+          this.token = params.get('token') as string;
 
-  private verify() {
-    this.activatedRoute.paramMap.subscribe(params => {
-      try {
-        const token = params.get('token');
-
-        this.authService.verifyEmail(token).subscribe({
-          next: () => {
-            this.status = 'Successfully verified';
-            this.cookieService.set('authToken', token as string);
-            this.authService.checkAuthentication();
-            this.router.navigateByUrl('/');
-          },
-          error: () => {
-            this.status = 'Something went wrong while verifying!';
-          },
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    });
+          return this.authService.verifyEmail(this.token);
+        })
+      )
+      .subscribe({
+        next: () => {
+          this.status = 'Successfully verified';
+          this.cookieService.set('authToken', this.token);
+          this.authService.checkAuthentication();
+          this.router.navigateByUrl('/');
+        },
+        error: () => {
+          this.status = 'Something went wrong while verifying!';
+        },
+      });
   }
 }
