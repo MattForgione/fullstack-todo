9d955a71956b40cb3114100ef60f6eee19d8d95f
MattForgione <mattforgionedev@gmail.com>
2022-12-07 11:52:29 -0400

    re-written verify-email so that it follows a similar solution for link expiry to password-reset, where on component load a check is carried out with the backend to manage whether the "link is expired"

--- todo-frontend/src/app/auth/verify-email/verify-email.component.ts
+++ todo-frontend/src/app/auth/verify-email/verify-email.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { CookieService } from 'ngx-cookie-service';
-import { mergeMap } from 'rxjs';
+import { map, mergeMap } from 'rxjs';
 
 @Component({
   selector: 'app-verify-email',
@@ -12,6 +12,7 @@ import { mergeMap } from 'rxjs';
 export class VerifyEmailComponent implements OnInit {
   status!: string;
   token!: string;
+  tokenExists!: boolean;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -26,19 +27,38 @@ export class VerifyEmailComponent implements OnInit {
         mergeMap(params => {
           this.token = params.get('token') as string;
 
-          return this.authService.verifyEmail(this.token);
+          return this.authService.checkTokenExists(this.token);
+        }),
+        mergeMap(({ tokenExists }) => {
+          this.tokenExists = tokenExists;
+
+          if (this.tokenExists) {
+            return this.router.navigateByUrl('/auth/link-has-expired');
+          }
+
+          return this.authService.storeUsedToken(this.token);
+        }),
+        map(() => {
+          if (!this.tokenExists) {
+            return this.authService.verifyEmail(this.token).subscribe({
+              next: () => {
+                if (!this.tokenExists) {
+                  this.status = 'Successfully verified';
+                  this.cookieService.set('authToken', this.token);
+                  this.authService.checkAuthentication();
+                  return this.router.navigateByUrl('/');
+                }
+
+                return;
+              },
+              error: () => {
+                this.status = 'Something went wrong while verifying!';
+              },
+            });
+          }
+          return;
         })
       )
-      .subscribe({
-        next: () => {
-          this.status = 'Successfully verified';
-          this.cookieService.set('authToken', this.token);
-          this.authService.checkAuthentication();
-          this.router.navigateByUrl('/');
-        },
-        error: () => {
-          this.status = 'Something went wrong while verifying!';
-        },
-      });
+      .subscribe();
   }
 }
