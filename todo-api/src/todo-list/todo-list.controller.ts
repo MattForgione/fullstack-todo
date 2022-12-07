f78453aff29be3b4d38c10226b88bf952c675f70
MattForgione <mattforgionedev@gmail.com>
2022-12-07 15:15:04 -0400

    wrote the create todo endpoint and service method

--- todo-api/src/todo-list/todo-list.controller.ts
+++ todo-api/src/todo-list/todo-list.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { TodoListService } from './todo-list.service';
 
 @Controller('todo-list')
@@ -9,5 +9,18 @@ export class TodoListController {
   async createTodoList(@Body() body: { title: string; email: string }) {
     const { title, email } = body;
     return this.todoListService.createTodoList(title, email);
+  }
+
+  @Post(':todoListId/todo')
+  async createTodo(
+    @Body() body: { title: string; content: string },
+    @Param('todoListId') todoListId: string
+  ) {
+    const { title, content } = body;
+    return this.todoListService.createTodo(
+      title,
+      content,
+      parseInt(todoListId)
+    );
   }
 }
--- todo-api/src/todo-list/todo-list.service.ts
+++ todo-api/src/todo-list/todo-list.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TodoList } from './todo-list.entity';
 import { Repository } from 'typeorm';
@@ -20,5 +20,21 @@ export class TodoListService {
     todoList.user = await this.usersService.findOne(email);
 
     return this.todoListRepository.save(todoList);
+  }
+
+  async createTodo(title: string, content: string, todoListId: number) {
+    const todoList = await this.todoListRepository.findOne({
+      where: { id: todoListId },
+    });
+
+    if (!todoList)
+      throw new NotFoundException(
+        `TodoList with id ${todoListId} was not found`
+      );
+
+    const todo = this.todoRepository.create({ title, content });
+    todo.todoList = todoList;
+
+    return this.todoRepository.save(todo);
   }
 }
