ddab218ccf561fb379981120c0dc06a62d31978c
MattForgione <mattforgionedev@gmail.com>
2022-12-08 11:31:43 -0400

    basic 404 page if route not found in the home module

--- todo-frontend/src/app/home/home-routing.module.ts
+++ todo-frontend/src/app/home/home-routing.module.ts
@@ -1,8 +1,19 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- todo-frontend/src/app/home/home.module.ts
+++ todo-frontend/src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, NotFoundComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
