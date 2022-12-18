d675561de1be1252c74e5c4d6a6625799b28a5f1
MattForgione <mattforgionedev@gmail.com>
2022-12-18 12:59:50 -0400

    experimented with jasmine unit tests and ran some basic tests

--- todo-frontend/src/app/app.component.spec.ts
+++ todo-frontend/src/app/app.component.spec.ts
@@ -1,17 +1,58 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HomeGuard } from './home.guard';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTabsModule } from '@angular/material/tabs';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatCardModule } from '@angular/material/card';
 
 describe('AppComponent', () => {
+  let loader: HarnessLoader;
+  let rootLoader: HarnessLoader;
+  let guard: HomeGuard;
+  let store: MockStore;
+  let fixture: ComponentFixture<AppComponent>;
+
+  const initialState = {
+    todos: {
+      todoLists: [],
+      currentlySelected: null,
+    },
+    app: {
+      currentNav: null,
+    },
+    auth: {
+      userSignedIn: false,
+    },
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        MatTabsModule,
+        MatCardModule,
+      ],
+      providers: [HomeGuard, provideMockStore({ initialState })],
       declarations: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    store = TestBed.inject(MockStore);
+    guard = TestBed.inject(HomeGuard);
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    rootLoader = TestbedHarnessEnvironment.documentRootLoader(fixture);
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
--- todo-frontend/src/app/auth/login/login.component.spec.ts
+++ todo-frontend/src/app/auth/login/login.component.spec.ts
@@ -1,22 +1,67 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthModule } from '../auth.module';
+import { AuthService } from '../auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from '../../shared/shared.module';
+import { By } from '@angular/platform-browser';
+import { FormComponent } from '../../shared/form/form.component';
+import { MatCardTitle } from '@angular/material/card';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let store: MockStore;
+
+  const initialState = {
+    todos: {
+      todoLists: [],
+      currentlySelected: null,
+    },
+    app: {
+      currentNav: null,
+    },
+    auth: {
+      userSignedIn: false,
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        AuthModule,
+        HttpClientModule,
+        SharedModule,
+      ],
       declarations: [LoginComponent],
+      providers: [AuthService, provideMockStore({ initialState })],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    store = TestBed.inject(MockStore);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a form component', () => {
+    const form = fixture.debugElement.query(By.directive(FormComponent));
+    expect(form).toBeTruthy();
+  });
+
+  it('should have "Login" as form title', () => {
+    const title = fixture.debugElement.query(By.directive(MatCardTitle));
+    expect(title.nativeElement.innerHTML).toEqual('Login');
   });
 });
