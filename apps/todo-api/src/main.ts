bda73572f79be8460fd0f0dc526b27076312b233
MattForgione <mattforgionedev@gmail.com>
2022-12-27 19:50:14 -0400

    final config for nestjs sub-application and removed old nestjs app folder

--- apps/todo-api/src/main.ts
+++ apps/todo-api/src/main.ts
@@ -10,13 +10,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
   app.enableCors();
-  app.setGlobalPrefix(globalPrefix);
   await app.listen(3000);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${3000}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${3000}`);
 }
 
 bootstrap();
--- todo-api/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../../apps/todo-api/src/app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
-}
-bootstrap();
