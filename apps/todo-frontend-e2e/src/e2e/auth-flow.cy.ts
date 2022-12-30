8b9dcd5808026bf898fe3fad39b9deabf40bcba6
MattForgione <mattforgionedev@gmail.com>
2022-12-30 02:10:55 -0400

    first draft of password reset test

--- apps/todo-frontend-e2e/src/e2e/auth-flow.cy.ts
+++ apps/todo-frontend-e2e/src/e2e/auth-flow.cy.ts
@@ -1,7 +1,9 @@
 const password = 'Testing12#';
+const passwordReset = 'Testing12#4';
 let inboxId: string;
 let emailAddress: string;
 let verifyEmailLink: string;
+let passwordResetLink: string;
 
 function dashboardSignPost() {
   return cy.get('h1.module-title--title').contains('Dashboard');
@@ -74,6 +76,36 @@ describe('Sign up flow with verification', () => {
 
       cy.reload();
       cy.get('mat-card-title').should('contain', 'Login');
+    });
+  });
+
+  context('Password reset flow works', () => {
+    it('should send a password reset email when email is passed to the reset form', () => {
+      cy.visit('/');
+      cy.get('a').contains('Click here to reset!').click();
+      cy.get('mat-label').contains('Email').type(emailAddress);
+      cy.get('span').contains('Send Email!').click();
+    });
+
+    it('should be able to visit the link, type in a new password, and submit, then log in', () => {
+      cy.waitForLatestEmail(inboxId).then(email => {
+        assert.isDefined(email);
+
+        const source = email.body as string;
+        // @ts-ignore
+        passwordResetLink = /<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/.exec(
+          source
+        )[2];
+        assert.isString(passwordResetLink);
+
+        cy.visit(passwordResetLink);
+        cy.get('mat-label').contains('Password').type(passwordReset);
+        cy.get('mat-label').contains('Confirm Password').type(passwordReset);
+        cy.get('span').contains('Reset!').click();
+
+        cy.loginFlow(emailAddress, passwordReset);
+        dashboardSignPost();
+      });
     });
   });
 });
